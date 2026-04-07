59d7b1724b5d0913d6c6295cb76451dfc9a9ae2e
netabakovv <netabakovv@yandex.ru>
2026-04-07 08:51:52 +0400

    with coverage >80%

--- main_test.go
+++ main_test.go
@@ -17,3 +17,47 @@ func TestHelloEndpoint(t *testing.T) {
 		t.Errorf("expected status 200, got %d", res.StatusCode)
 	}
 }
+func TestHelloEndpoint1(t *testing.T) {
+	req := httptest.NewRequest("GET", "/api/hello1", nil)
+	w := httptest.NewRecorder()
+
+	helloHandler(w, req)
+
+	res := w.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status 200, got %d", res.StatusCode)
+	}
+}
+func TestHelloEndpoint2(t *testing.T) {
+	req := httptest.NewRequest("GET", "/api/hello2", nil)
+	w := httptest.NewRecorder()
+
+	helloHandler(w, req)
+
+	res := w.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status 200, got %d", res.StatusCode)
+	}
+}
+func TestHelloEndpoint3(t *testing.T) {
+	req := httptest.NewRequest("GET", "/api/hello3", nil)
+	w := httptest.NewRecorder()
+
+	helloHandler(w, req)
+
+	res := w.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status 200, got %d", res.StatusCode)
+	}
+}
+func TestHelloEndpoint4(t *testing.T) {
+	req := httptest.NewRequest("GET", "/api/hello4", nil)
+	w := httptest.NewRecorder()
+
+	helloHandler(w, req)
+
+	res := w.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status 200, got %d", res.StatusCode)
+	}
+}
