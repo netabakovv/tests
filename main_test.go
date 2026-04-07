fa6124739a3e0fed0ea192a29171e18fddbb0c34
netabakovv <netabakovv@yandex.ru>
2026-04-07 09:12:25 +0400

    +docker img

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHelloEndpoint(t *testing.T) {
 
 	res := w.Result()
 	if res.StatusCode != http.StatusOK {
-		t.Errorf("expected status 200, got %d", res.StatusCode)
+		t.Errorf("expected status 200, gott %d", res.StatusCode)
 	}
 }
 
